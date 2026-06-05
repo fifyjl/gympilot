ff2409c6f710d70fc4912cf57e78c256fcd7c5a8
fifyjl <fifyjl@users.noreply.github.com>
2026-06-05 10:11:40 +0800

    Rename Pages base path to gympolit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fitnessApiPlugin } from './server/fitnessApi.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sports/',
+  base: '/gympolit/',
   plugins: [react(), fitnessApiPlugin()],
   server: {
     allowedHosts: ['.trycloudflare.com', '.loca.lt'],
